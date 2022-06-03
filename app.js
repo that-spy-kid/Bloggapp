39f56d19eff9e0b22126035d22fc8454a05d844d
Vinay Kumar <vinay173.viit@gmail.com>
2022-06-03 17:04:51 +0530

    ✅

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const homeStartingContent = "Daily Journal is Blog app, in which we can able to post our day-to-day updates and describes various innovative content and to share the key insights.";
 const aboutContent = "We, the daily journal is a blog post site that which we can able to post the content on various interests topics  and can also able to post our  personal experiences and many more individual insights to help peers and to aware & guided them in good direction. ";
-const contactContent = "We hope that this blog site would help you out in some way that we can take necessary steps to do something in individual interest , please try to post some more blogs which would help for many more individuals.You can post blog any time by just navigating to /compose from home page, so what're you looking for,  post your blog right now !";
+const contactContent = "We hope that this blog site would help you out in some way that we can take necessary steps to do something in individual interest , please try to post some more blogs which would help for many more individuals.You can post blog any time by just navigating to /compose from home page or else click above /compose to post straight away, so what're you waiting for,  post your blog right now!";
 
 const app = express();
 
